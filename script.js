bd4da8c435a655848486a62c775f23a6e9ed6c2a
timranm-stack <timranm@ikaslagun.org>
2026-02-01 12:55:55 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-function myFunction(){
-    var a = 4;
-    document.getElementById("demo"). innerHTML = a*a;
+function handleContact(){
+    var fullName = document.getElementById("fullname").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    var snackbar = document.getElementById("snackbar");
+    var snackbarText = "";
+    var snackbarBG = "#28a7e9";
+
+    if((fullName === "") (email === "") (message === "") ){
+        snackbarText = "Please enter values for all fields";
+        snackbarBG = "rbg(255, 204, 203)";
+    }
+    else{
+        snackbar.className = "show";
+        snackbarText = "Thanks ${fullName}...Your message has been recorded";
+    }
+    snackbar.className = "show";
+    snackbar.innerHTML = snackbarText;
+    snackbar.style
 }
